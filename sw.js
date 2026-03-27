fb9067c2d59d8f05cf2b63a800161ea953454a30
RDahunan IT Services <rdahunanitservices@gmail.com>
2026-03-27 12:39:13 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
