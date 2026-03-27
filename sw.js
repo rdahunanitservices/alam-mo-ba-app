b76e54ed203c89e61fe7e0d025ca5593707eb6ce
RDahunan IT Services <rdahunanitservices@gmail.com>
2026-03-27 12:44:19 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,24 @@
-self.addEventListener('install', e => {
+const CACHE_NAME = "alam-mo-ba-v1";
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./app-icon-512x512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  return self.clients.claim();
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
